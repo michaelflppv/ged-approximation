7b8b519d4f843b8db3e2fba8832bf76e369ebc23
mikef <mikhail.filippov@students.uni-mannheim.de>
2025-02-14 23:11:44 +0100

    Added saving the trained model

--- SimGNN/src/main.py
+++ SimGNN/src/main.py
@@ -1,5 +1,6 @@
 """SimGNN runner."""
 
+import os
 from utils import tab_printer
 from simgnn import SimGNNTrainer
 from param_parser import parameter_parser
@@ -12,13 +13,22 @@ def main():
     args = parameter_parser()
     tab_printer(args)
     trainer = SimGNNTrainer(args)
+
     if args.load_path:
         trainer.load()
     else:
         trainer.fit()
     #trainer.score()
-    if args.save_path:
-        trainer.save()
+
+    # Set default save path relative to the src folder if not provided.
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    models_dir = os.path.join(script_dir, "..", "models")
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+    if not args.save_path:
+        args.save_path = os.path.join(models_dir, "simgnn_model.pth")
+
+    trainer.save()
 
 if __name__ == "__main__":
     main()
