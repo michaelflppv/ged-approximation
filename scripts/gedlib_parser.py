6c54596353256eae0e78c9055aed6870fa3be504
michaelflppv <mikhail.filippov@students.uni-mannheim.de>
2025-02-18 13:50:21 +0100

    Update Results Analysis

--- scripts/gedlib_parser.py
+++ scripts/gedlib_parser.py
@@ -14,10 +14,11 @@ GED_EXECUTABLE = "/home/mfilippov/CLionProjects/gedlib/build/main_exec"  # Path 
 DATASET_PATH = "../processed_data/gxl/PROTEINS"  # Path to PROTEINS dataset directory
 COLLECTION_XML = "../processed_data/xml/PROTEINS.xml"  # Path to PROTEINS collection XML file
 RESULTS_DIR = "../results/gedlib"  # Directory to save GEDLIB results
-RESULTS_FILE = os.path.join(RESULTS_DIR, "PROTEINS_results.xlsx")  # Path to save results
+RESULTS_FILE = os.path.join(RESULTS_DIR, "PROTEINS_IPFP_results.xlsx")  # Path to save results
 
 # Update method mapping according to new C++ enum values (assumed to be 0,1,2,3)
 METHOD_NAMES = {
+    0: "F2",
     20: "STAR (Exact)",
     10: "IPFP",
     11: "BIPARTITE",
